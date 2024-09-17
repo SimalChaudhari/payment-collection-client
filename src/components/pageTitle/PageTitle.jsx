457dd915de63a24d06b9ff53e368f0bd9f869d0a
zangula-dev <simalc@zangula.com>
2024-09-17 16:28:23 +0530

    add

--- src/components/pageTitle/PageTitle.jsx
+++ src/components/pageTitle/PageTitle.jsx
@@ -5,17 +5,20 @@ import { useLocation } from 'react-router-dom';
 import { adminRoutes, salesmanRoutes, customerRoutes, authRoutes } from '@/routes';
 
 const getTitle = (pathname) => {
-    console.log("🚀 ~ getTitle ~ pathname:", pathname)
+
     const allRoutes = [...adminRoutes.flatMap(route => route.pages),
     ...salesmanRoutes.flatMap(route => route.pages),
     ...customerRoutes.flatMap(route => route.pages),
     ...authRoutes.flatMap(route => route.pages)];
 
-    const route = allRoutes.find(route => {
-        console.log("🚀 ~ getTitle ~ route:", route)
-        return route.path
-    })
-    return route ? `Payment Collection | ${route.name}` : "Payment Collection";
+    const lastPart = pathname.split('/').pop().toLowerCase(); // Extract 'salesmans' or 'customers' and convert to lowercase
+    // Check if any route matches the last part of the path
+    const matchedRoute = allRoutes.find((route) => route.name.toLowerCase() === lastPart);
+    if (matchedRoute) {
+        return matchedRoute ? `Payment Collection | ${matchedRoute.name}` : "Payment Collection";
+    } else {
+        console.log("No matching route found.");
+    }
 };
 
 const PageTitle = () => {
--- src/routes.jsx
+++ src/routes.jsx
@@ -34,7 +34,7 @@ export const adminRoutes = [
     pages: [
       { icon: <HomeIcon {...icon} />, name: "Dashboard", path: "/dashboard", element: <Home /> },
       { icon: <UserIcon {...icon} />, name: "Customers", path: "/customers", element: <View /> },
-      { icon: <UserCircleIcon {...icon} />, name: "Salesman's", path: "/salesmans", element: <ViewSalesman /> },
+      { icon: <UserCircleIcon {...icon} />, name: "Salesmans", path: "/salesmans", element: <ViewSalesman /> },
       { icon: <CreditCardIcon {...icon} />, name: "Payments", path: "/payments", element: <Payment /> },
       { icon: <InformationCircleIcon {...icon} />, name: "Reports", path: "/reports", element: <ReportData /> },
       { icon: <UserCircleIcon {...icon} />, name: "Profile", path: "/profile", element: <AdminProfile /> },
@@ -49,8 +49,8 @@ export const salesmanRoutes = [
   {
     layout: "salesman",
     pages: [
-      { icon: <HomeIcon {...icon} />, name: "dashboard", path: "/dashboard", element: <HomeSalesman /> },
-      { icon: <UserIcon {...icon} />, name: "Collection", path: "/collections", element: <Collection /> },
+      { icon: <HomeIcon {...icon} />, name: "Dashboard", path: "/dashboard", element: <HomeSalesman /> },
+      { icon: <UserIcon {...icon} />, name: "Collections", path: "/collections", element: <Collection /> },
       { icon: <UserCircleIcon {...icon} />, name: "Profile", path: "/profile", element: <AdminProfile /> },
       // { path: "*", element: <NotFound /> }, // 404 Page
     ],
@@ -61,8 +61,8 @@ export const customerRoutes = [
   {
     layout: "customer",
     pages: [
-      { icon: <HomeIcon {...icon} />, name: "dashboard", path: "/dashboard", element: <CustomerDashboard /> },
-      { icon: <CreditCardIcon {...icon} />, name: "Verify Payments", path: "/verify", element: <PaymentVerify /> },
+      { icon: <HomeIcon {...icon} />, name: "Dashboard", path: "/dashboard", element: <CustomerDashboard /> },
+      { icon: <CreditCardIcon {...icon} />, name: "Payments", path: "/payments", element: <PaymentVerify /> },
       { icon: <UserCircleIcon {...icon} />, name: "Profile", path: "/profile", element: <AdminProfile /> },
       // { path: "*", element: <NotFound /> }, // 404 Page
     ],
